2d1e60167f19c8b5f81f97c277eb9b5377c15f10
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2023-03-30 15:20:51 +0530

    prototype

--- script3.js
+++ script3.js
@@ -1,99 +1,131 @@
-let arr = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-];
-const inputCaptcha = document.getElementsByClassName("input");
-const reloadBtn = document.querySelector(".reload-btn");
-console.log(inputCaptcha);
-console.log(reloadBtn);
+// let arr = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+//   "G",
+//   "H",
+//   "I",
+//   "J",
+//   "K",
+//   "L",
+//   "M",
+//   "N",
+//   "O",
+//   "P",
+//   "Q",
+//   "R",
+//   "S",
+//   "T",
+//   "U",
+//   "V",
+//   "W",
+//   "X",
+//   "Y",
+//   "Z",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+//   6,
+//   7,
+//   8,
+//   9,
+//   10,
+// ];
+// const inputCaptcha = document.getElementsByClassName("input");
+// const reloadBtn = document.querySelector(".reload-btn");
+// console.log(inputCaptcha);
+// console.log(reloadBtn);
 
 
-function generateRandomCaptcha() {
-  for (let i = 0; i <= 5; i++) {
-    let val = arr[Math.floor(Math.random() * arr.length)];
+// function generateRandomCaptcha() {
+//   for (let i = 0; i <= 5; i++) {
+//     let val = arr[Math.floor(Math.random() * arr.length)];
   
-    inputCaptcha[0].innerText += `${val}`;
-  }
-}
+//     inputCaptcha[0].innerText += `${val}`;
+//   }
+// }
 
-reloadBtn.addEventListener("click", (e) => {
-  inputCaptcha[0].innerText=" "
-  generateRandomCaptcha();
+// reloadBtn.addEventListener("click", (e) => {
+//   inputCaptcha[0].innerText=" "
+//   generateRandomCaptcha();
   
 
-});
+// });
 
 
-function callBack(){
-console.log("Callback called")
+// function callBack(){
+// console.log("Callback called")
+// }
+
+// function call2(param){
+//   console.log("callback function called below")
+// param()
+// }
+
+// call2(callBack)
+
+// const evaluate= eval("2+3")
+// console.log(typeof (typeof String))
+
+// let counter = 0;
+// const fetchData = ()=>{
+//   console.log("key pressed", counter++)
+// }
+
+
+// const debouncingFunction = ()=>{
+//   let timer;
+//   return ()=> {
+
+//    clearTimeout(timer)
+//    timer = setTimeout(()=>{
+//       fetchData() 
+//     }, 300)
+//   }
+// }
+
+// const newFunc = debouncingFunction()
+
+const arr = [1, 2, 3,4, 5]
+
+const obj = {
+  firstName: 'John',
+  lastName: "Cena"
 }
 
-function call2(param){
-  console.log("callback function called below")
-param()
+const obj2 = {
+  firstname: "lakshay"
 }
 
-call2(callBack)
-
-const evaluate= eval("2+3")
-console.log(typeof (typeof String))
+obj2.__proto__ = obj
